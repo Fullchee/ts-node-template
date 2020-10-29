ad8d09a94b97610b2b819563e7586a4a92bf5b57
Fullchee Zhang <fullchee@gmail.com>
2020-10-28 21:15:17 -0400

    Add complete this

--- src/3sum.ts
+++ src/3sum.ts
@@ -1,9 +1,41 @@
+function threeSum(nums: number[]): number[][] {
+  const result: number[][] = [];
+
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length - 2; i++) {
+    // avoid duplicates, thanks to sorting!
+    if (i !== 0 && nums[i] === nums[i - 1]) {
+      continue;
+    }
+
+    let low = i + 1;
+    let high = nums.length - 1;
+
+    // increment low if sum < 0
+    // decrement high if sum > 0
+    while (low < high) {
+      const sum = nums[i] + nums[low] + nums[high];
+      if (sum === 0) {
+        // avoid duplicates here too!
+        result.push([nums[i], nums[low], nums[high]]);
+        while (low < high && nums[low] === nums[low + 1]) low++;
+        while (low < high && nums[high] === nums[high - 1]) high--;
+      } else if (sum < 0) {
+        low++;
+      } else {
+        high++;
+      }
+    }
+  }
+  return result;
+}
+
 type Pair = {
-    index1: number;
-    value1: number;
-    index2: number;
-    value2: number;
-  };
+  index1: number;
+  value1: number;
+  index2: number;
+  value2: number;
+};
 
 function threeSumInefficient(nums: number[]): number[][] {
   let stringResult: string[] = [];
@@ -16,14 +48,14 @@ function threeSumInefficient(nums: number[]): number[][] {
       pairs.forEach(({ index1, value1, index2, value2 }) => {
         // check for duplicates
         if (i !== index1 && i !== index2) {
-            stringResult.push(JSON.stringify([value1, value2, num].sort()));
+          stringResult.push(JSON.stringify([value1, value2, num].sort()));
         }
       });
     }
   }
   stringResult = [...new Set(stringResult)];
 
-  return stringResult.map(res => {
+  return stringResult.map((res) => {
     return JSON.parse(res);
   });
 }
@@ -37,13 +69,11 @@ function getSums(nums: number[]) {
       if (sums.has(a + b)) {
         sums.get(a + b).push({ index1: i, value1: a, index2: j, value2: b });
       } else {
-        sums.set(a + b, [
-            { index1: i, value1: a, index2: j, value2: b },
-        ]);
+        sums.set(a + b, [{ index1: i, value1: a, index2: j, value2: b }]);
       }
     }
   }
   return sums;
 }
 
-console.log(threeSum([-1,0,1,2,-1,-4,-2,-3,3,0,4]));
+console.log(threeSum([-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]));
